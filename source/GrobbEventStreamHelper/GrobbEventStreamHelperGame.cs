3400452a2d7ae134fa3a89e13a17028a247771a0
Gary <gtexmo@gmail.com>
2024-11-22 09:21:46 -0800

    Initial commit.

---
+++ source/GrobbEventStreamHelper/GrobbEventStreamHelperGame.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace GrobbEventStreamHelper
+{
+    public class GrobbEventStreamHelperGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public GrobbEventStreamHelperGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: Use this.Content to load your game content here
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ source/GrobbEventStreamHelper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GrobbEventStreamHelper
+{
+#if  WINDOWSDX || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GrobbEventStreamHelperGame())
+                game.Run();
+        }
+    }
+#endif
+}
